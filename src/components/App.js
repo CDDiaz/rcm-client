76bde1a16d2f53ac0369fd44a570abddca9d9276
roweeena <roe0320@gmail.com>
2021-08-11 11:48:48 +1000

    routes and airplanes.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,46 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Airplanes from './Airplanes'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/airplanes">Airplanes</Link>
+            </li>
+          </ul>
+        </nav>
 
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Airplanes">
+            <Airplanes />
+          </Route>
+
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+
+    return (
+      <h2>Home</h2>
+    )
+
+
+}
